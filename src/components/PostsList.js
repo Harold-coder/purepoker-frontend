73dab319b7116f269163db80d9e322e404260509
Harold-coder <hjc2154@columbia.edu>
2024-01-18 17:41:35 -0500

    Delete functionality worksgit add .

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -16,11 +16,26 @@ const PostsList = ( { posts, setPosts }) => {
             .catch(error => console.error('Error updating like:', error));
     };
 
+    const handleDelete = (postId) => {
+        axios.delete(`${urlServer}/posts/${postId}`)
+            .then(() => {
+                // Filter out the deleted post
+                const updatedPosts = posts.filter(post => post.id !== postId);
+                setPosts(updatedPosts);
+            })
+            .catch(error => console.error('Error deleting post:', error));
+    };
+
     return (
         <div className="posts-list">
             {posts.map(post => (
                 <div key={post.id} className="post">
-                <h3>{post.author}</h3>
+                <div className="post-header">
+                    <h3>{post.author}</h3>
+                    <span className="delete-icon" onClick={() => handleDelete(post.id)}>
+                        <i className="fas fa-trash-alt"></i>
+                    </span>
+                </div>
                 <p>{post.content}</p>
                 <div className="post-interactions">
                     <span className="likes" onClick={() => handleLike(post)}>
