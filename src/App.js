97317f86173df44b6fe2de714c8a96ec1d1df425
Harold-coder <hjc2154@columbia.edu>
2024-02-05 14:38:48 -0500

    Changed the color to wine red and changed the url to the gateway one.

--- src/App.js
+++ src/App.js
@@ -13,13 +13,14 @@ import SignUp from './components/SignUp';
 
 import { useAuth } from './context/AuthContext';
 
-export const urlServer = "http://127.0.0.1:8012";
+// export const urlServer = "http://127.0.0.1:8012";
+export const urlServer = "https://b03ruvxz55.execute-api.us-east-1.amazonaws.com/dev";
 export const urlServerAuth = "http://127.0.0.1:8013";
 
 const ProtectedRoute = ({ children }) => {
-    const { user } = useAuth();
-    return user ? children : <Navigate to="/login" />;
-    // return children;
+    // const { user } = useAuth();
+    // return user ? children : <Navigate to="/login" />;
+    return children;
 };
 
 function App() {
